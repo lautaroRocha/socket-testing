394e86f9e6fc71332602a375369ee11ce18d6023
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-04 13:43:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+
+
+const io = new Server(httpServer,  {
+    cors: {
+        origin: "http://localhost:3000/"
+    }});
+
+    io.on('connection', (socket) => {
+        console.log(`⚡: ${socket.id} user just connected!`);
+        socket.on('disconnect', () => {
+          console.log('🔥: A user disconnected');
+        });
+        socket.on('new-confirmed', (socket) => {
+            io.emit('confirmed-notification', JSON.stringify(socket))
+        });
+        socket.on('new-answer', (socket) => {
+            io.emit('answer-notification', JSON.stringify(socket))
+        });
+        socket.on('new-liked', (socket) => {
+            io.emit('like-notification', JSON.stringify(socket))
+        });
+    });
+
+httpServer.listen(port);
